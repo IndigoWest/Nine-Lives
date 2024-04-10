d2caf1336046ab73e2d3426a6dcb2e0d92622c11
IndigoWest <114085111+IndigoWest@users.noreply.github.com>
2024-04-10 10:46:45 -0500

    Working on Sliders

---
+++ Assets/Scripts/Brightness Manager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessManager : MonoBehaviour
+{
+    private static BrightnessManager instance;
+
+    public static BrightnessManager Instance { get { return instance; } }
+
+    
+}
